e143a03156bb9c4ab7eac5303e63c18b514ac132
smallcolin <colinsweeney2000@gmail.com>
2018-01-14 21:55:27 +0100

    added instagram fallback msg if no internet connection exists

--- photos.php
+++ photos.php
@@ -16,25 +16,29 @@
 					$json_link = 'https://api.instagram.com/v1/users/self/media/recent/?';
 					$json_link .= "access_token={$access_token}&count={$photo_count}";
 
-					$json = file_get_contents($json_link);
+					$json = @file_get_contents($json_link);
 					$obj = json_decode(preg_replace('/("\w+"):(\d+)/', '\\1:"\\2"', $json), true);
 
-					foreach ($obj['data'] as $post) {
-				    $title = $post['caption']['text'];
-				    $pic_link = $post['link'];
-				    $likes = $post['likes']['count'];
-				    $pic_src = str_replace("http://", "https://", $post['images']['standard_resolution']['url']);
-				    $pic_created_time = date("F j, Y", $post['caption']['created_time']);
-				    $pic_created_time = date("M j, Y", strtotime($pic_created_time . " +1 days"));
+					if ($json == false) {
+						echo '*To access the instagram feed you will need to connect to the internet*';
+					} else {
+						foreach ($obj['data'] as $post) {
+							$title = $post['caption']['text'];
+							$pic_link = $post['link'];
+							$likes = $post['likes']['count'];
+							$pic_src = str_replace("http://", "https://", $post['images']['standard_resolution']['url']);
+							$pic_created_time = date("F j, Y", $post['caption']['created_time']);
+							$pic_created_time = date("M j, Y", strtotime($pic_created_time . " +1 days"));
 
-				    echo "<div class='col-md-3 col-sm-4 col-xs-12 bilder-deets'>";
-			        echo "<a href='{$pic_link}' target='_blank'>";
-	            echo "<img class='img-responsive photo-thumb' src='{$pic_src}' alt='{$title}'>";
-			        echo "</a>";
-	            echo "<p>{$title}</p>";
+							echo "<div class='col-md-3 col-sm-4 col-xs-12 bilder-deets'>";
+							echo "<a href='{$pic_link}' target='_blank'>";
+							echo "<img class='img-responsive photo-thumb' src='{$pic_src}' alt='{$title}'>";
+							echo "</a>";
+							echo "<p>{$title}</p>";
 							echo "<p>{$pic_created_time}</p>";
 							echo "<p><i class='fa fa-heart'></i>{$likes}</p>";
-				    echo "</div>";
+							echo "</div>";
+						}
 					}
 				?>
 			</div>
