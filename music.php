0a8c8bc9cfcfd3cbdce1bf8d0a5711514962a3b7
Colin Sweeney <colinsweeney2000@gmail.com>
2018-02-23 18:42:25 +0100

    changing music layout to flex. unfinished

--- music.php
+++ music.php
@@ -7,62 +7,64 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-xs-12 col-sm-3">
-				<div class="album">
-					<img src="assets/img/retro_masters.jpg" alt="retro">
-					<div class="post-info">
-						<h4 class="post-title">
-							<a href="https://smallcolin.bandcamp.com/album/retro-masters" target="_blank">
-								<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-							</a>
-						</h4>
+			<div style="display:flex;flex-wrap:wrap;">
+				<div style="width:50%;">
+					<div class="album">
+						<img src="assets/img/retro_masters.jpg" alt="retro">
+						<div class="post-info">
+							<h4 class="post-title">
+								<a href="https://smallcolin.bandcamp.com/album/retro-masters" target="_blank">
+									<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+								</a>
+							</h4>
+						</div>
+						<div class="image-overlay"></div>
 					</div>
-					<div class="image-overlay"></div>
+					<h4>Retro Masters (2011)</h4>
 				</div>
-				<h4>Retro Masters (2011)</h4>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="album">
-					<img src="assets/img/tape_productions.jpg" alt="tape">
-					<div class="post-info">
-						<h4 class="post-title">
-							<a href="https://smallcolin.bandcamp.com/album/tape-productions" target="_blank">
-								<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-							</a>
-						</h4>
+				<div style="width:50%;">
+					<div class="album">
+						<img src="assets/img/tape_productions.jpg" alt="tape">
+						<div class="post-info">
+							<h4 class="post-title">
+								<a href="https://smallcolin.bandcamp.com/album/tape-productions" target="_blank">
+									<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+								</a>
+							</h4>
+						</div>
+						<div class="image-overlay"></div>
 					</div>
-					<div class="image-overlay"></div>
+					<h4>Tape Productions (2013)</h4>
 				</div>
-				<h4>Tape Productions (2013)</h4>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="album">
-					<img src="assets/img/mono_box.jpg" alt="mono">
-					<div class="post-info">
-						<h4 class="post-title">
-							<a href="https://smallcolin.bandcamp.com/album/mono-box" target="_blank">
-								<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-							</a>
-						</h4>
+				<div style="width:50%;">
+					<div class="album">
+						<img src="assets/img/mono_box.jpg" alt="mono">
+						<div class="post-info">
+							<h4 class="post-title">
+								<a href="https://smallcolin.bandcamp.com/album/mono-box" target="_blank">
+									<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+								</a>
+							</h4>
+						</div>
+						<div class="image-overlay"></div>
 					</div>
-					<div class="image-overlay"></div>
+					<h4>Mono Box (2014)</h4>
 				</div>
-				<h4>Mono Box (2014)</h4>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="album">
-					<img src="assets/img/loonies.jpg" alt="loonies">
-					<div class="post-info">
-						<h4 class="post-title">
-							<a href="https://rsvptapes.bandcamp.com/album/small-colin-loonball-rsvp013" target="_blank">
-								<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-							</a>
-						</h4>
+				<div style="width:50%;">
+					<div class="album">
+						<img src="assets/img/loonies.jpg" alt="loonies">
+						<div class="post-info">
+							<h4 class="post-title">
+								<a href="https://rsvptapes.bandcamp.com/album/small-colin-loonball-rsvp013" target="_blank">
+									<span class="white-text">Listen…</span> <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+								</a>
+							</h4>
+						</div>
+						<div class="image-overlay"></div>
 					</div>
-					<div class="image-overlay"></div>
+					<h4>Loonball (2017)</h4>
 				</div>
-				<h4>Loonball (2017)</h4>
 			</div>
-		</div>
+	</div>
 	</div>
 </section>
