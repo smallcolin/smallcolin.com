9a02f94dc550680df674a5d6db81119138c16d84
smallcolin <colinsweeney2000@gmail.com>
2018-03-03 16:17:42 +0100

    changed website images and added flexbox

--- websites.php
+++ websites.php
@@ -2,74 +2,86 @@
 	<div class="container">
 		<div class="row">
 			<h3 class="animated flipInX">Websites</h3>
-			<div class="col-xs-12 col-sm-6">
-
-				<div class="site-container">
-					<h4 class="animated flipInX">
-						<a href="http://kappabar.se/" target="_blank">
-							kappabar.se
-						</a>
-					</h4>
-					<p class="move-text">Wordpress | ACF | jQuery</p>
-					<div class="box">
-						<a href="http://kappabar.se/" target="_blank">
-							<img src="assets/img/kappabar.png" />
-						</a>
+				<div class="work">
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://kappabar.se/" target="_blank">
+								kappabar.se
+							</a>
+						</h4>
+						<p class="move-text">Wordpress | ACF | jQuery | PHP | Ajax</p>
+						<div class="box">
+							<a href="http://kappabar.se/" target="_blank">
+								<img src="assets/img/kappabar.se_responsive.jpg" />
+							</a>
+						</div>
+					</div>
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://smallcolin.se/" target="_blank">
+								smallcolin.se
+							</a>
+						</h4>
+						<p class="move-text">HTML / CSS / JQuery</p>
+						<div class="box">
+							<a href="http://smallcolin.se/" target="_blank">
+								<img src="assets/img/smallcolin.se_responsive.jpg" />
+							</a>
+						</div>
+					</div>
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://bittrabritta.se" target="_blank">
+								bittrabritta.se
+							</a>
+						</h4>
+						<p class="move-text">Wordpress | ACF | jQuery</p>
+						<div class="box">
+							<a href="http://saefridarbraga.com" target="_blank">
+								<img src="assets/img/bittrabritta.se.jpg" />
+							</a>
+						</div>
+					</div>
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://saefridarbraga.com" target="_blank">
+								saefridarbraga.com
+							</a>
+						</h4>
+						<p class="move-text">Wordpress | ACF | jQuery | PHP</p>
+						<div class="box">
+							<a href="http://saefridarbraga.com" target="_blank">
+								<img src="assets/img/saefridarbraga.com.jpg" />
+							</a>
+						</div>
+					</div>
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://bittrabritta.se" target="_blank">
+								beardlessbrewery.se
+							</a>
+						</h4>
+						<p class="move-text">PHP | jQuery </p>
+						<div class="box">
+							<a href="http://beardlessbrewery.se" target="_blank">
+								<img src="assets/img/beardlessbrewery.se.jpg" />
+							</a>
+						</div>
+					</div>
+					<div class="site-container">
+						<h4 class="animated flipInX">
+							<a href="http://saefridarbraga.com" target="_blank">
+								hanshugos.se
+							</a>
+						</h4>
+						<p class="move-text">Wordpress | Woocommerce | jQuery | PHP</p>
+						<div class="box">
+							<a href="http://hanshugos.se/" target="_blank">
+								<img src="assets/img/hanshugos.se_responsive.jpg" />
+							</a>
+						</div>
 					</div>
 				</div>
-
-			</div>
-			<div class="col-xs-12 col-sm-6">
-
-				<div class="site-container">
-					<h4 class="animated flipInX">
-						<a href="http://smallcolin.se/" target="_blank">
-							smallcolin.se
-						</a>
-					</h4>
-					<p class="move-text">HTML / CSS / JQuery</p>
-					<div class="box">
-						<a href="http://smallcolin.se/" target="_blank">
-							<img src="assets/img/smallcolin_se.png" />
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12 col-sm-6">
-
-				<div class="site-container">
-					<h4 class="animated flipInX">
-						<a href="http://bittrabritta.se" target="_blank">
-							bittrabritta.se
-						</a>
-					</h4>
-					<p class="move-text">A wordpress based website about furnishings</p>
-					<div class="box">
-						<a href="http://saefridarbraga.com" target="_blank">
-							<img src="assets/img/bittra.png" />
-						</a>
-					</div>
-				</div>
-
-			</div>
-			<div class="col-xs-12 col-sm-6">
-
-				<div class="site-container">
-					<h4 class="animated flipInX">
-						<a href="http://saefridarbraga.com" target="_blank">
-							saefridarbraga.com
-						</a>
-					</h4>
-					<p class="move-text">An icelandic family tree (wordpress)</p>
-					<div class="box">
-						<a href="http://saefridarbraga.com" target="_blank">
-							<img src="assets/img/saefridarbraga.png" />
-						</a>
-					</div>
-				</div>
-
 			</div>
 		</div>
 	</div>
