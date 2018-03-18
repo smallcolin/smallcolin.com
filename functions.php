94b97e696d638726874e7158bc0af3ceb792492b
smallcolin <colinsweeney2000@gmail.com>
2018-03-18 22:57:44 +0100

    simplified websites page

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+
+  // Website Data
+
+  $wb_data = [
+    'sites' => [
+      [
+        'url' => 'http://kappabar.se/',
+        'short_url' => 'kappabar.se',
+        'skills' => [
+          'Wordpress',
+          'ACF',
+          'jQuery',
+          'PHP',
+          'Ajax'
+        ],
+        'image' => 'assets/img/kappabar.se_responsive.jpg'
+      ],
+      [
+        'url' => 'https://bittrabritta.se/',
+        'short_url' => 'bittrabritta.se',
+        'skills' => [
+          'Wordpress',
+          'ACF',
+          'jQuery',
+          'PHP'
+        ],
+        'image' => 'assets/img/bittrabritta.se.jpg'
+      ],
+      [
+        'url' => 'http://saefridarbraga.com',
+        'short_url' => 'saefridarbraga.com',
+        'skills' => [
+          'Wordpress',
+          'ACF',
+          'jQuery',
+          'PHP'
+        ],
+        'image' => 'assets/img/saefridarbraga.com.jpg'
+      ],
+      [
+        'url' => 'http://hanshugos.se/',
+        'short_url' => 'hanshugos.se',
+        'skills' => [
+          'Wordpress',
+          'Woocommerce',
+          'jQuery',
+          'PHP'
+        ],
+        'image' => 'assets/img/hanshugos.se_responsive.jpg'
+      ],
+      [
+        'url' => 'https://github.com/smallcolin/wine',
+        'short_url' => 'wine ecommerce',
+        'skills' => [
+          'Laravel',
+          'PHP'
+        ],
+        'image' => 'assets/img/wine-test.jpg'
+      ],
+      [
+        'url' => 'http://smallcolin.se/',
+        'short_url' => 'smallcolin.se',
+        'skills' => [
+          'HTML',
+          'CSS',
+          'jQuery'
+        ],
+        'image' => 'assets/img/smallcolin.se_responsive.jpg'
+      ],
+    ] 
+  ];
--- websites.php
+++ websites.php
@@ -1,86 +1,37 @@
+<?php include 'functions.php'; ?>
+
 <section id="projects" class="websites">
 	<div class="container">
 		<div class="row">
 			<h3 class="animated flipInX">Projects</h3>
 				<div class="work">
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="http://kappabar.se/" target="_blank">
-								kappabar.se
-							</a>
-						</h4>
-						<p class="move-text">Wordpress | ACF | jQuery | PHP | Ajax</p>
-						<div class="box">
-							<a href="http://kappabar.se/" target="_blank">
-								<img src="assets/img/kappabar.se_responsive.jpg" />
-							</a>
+					<?php foreach ($wb_data['sites'] as $site) { ?>
+						<div class="site-container">
+							<h4 class="animated flipInX">
+								<a href="<?php echo $site['url']; ?>" target="_blank">
+									<?php echo $site['short_url']; ?>
+								</a>
+							</h4>
+							<p class="move-text">
+								<?php 
+									$count = count($site['skills']);
+									$i = 1;
+									foreach ($site['skills'] as $skill) {
+										echo $skill;
+										if($i < $count) {
+											echo ' | ';
+											$i++;
+										};
+									} 
+								?>
+							</p>
+							<div class="box">
+								<a href="<?php echo $site['url']; ?>" target="_blank">
+									<img src="<?php echo $site['image']; ?>" />
+								</a>
+							</div>
 						</div>
-					</div>
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="https://bittrabritta.se" target="_blank">
-								bittrabritta.se
-							</a>
-						</h4>
-						<p class="move-text">Wordpress | ACF | jQuery</p>
-						<div class="box">
-							<a href="https://bittrabritta.se" target="_blank">
-								<img src="assets/img/bittrabritta.se.jpg" />
-							</a>
-						</div>
-					</div>
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="http://saefridarbraga.com" target="_blank">
-								saefridarbraga.com
-							</a>
-						</h4>
-						<p class="move-text">Wordpress | ACF | jQuery | PHP</p>
-						<div class="box">
-							<a href="http://saefridarbraga.com" target="_blank">
-								<img src="assets/img/saefridarbraga.com.jpg" />
-							</a>
-						</div>
-					</div>
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="http://hanshugos.se/" target="_blank">
-								hanshugos.se
-							</a>
-						</h4>
-						<p class="move-text">Wordpress | Woocommerce | jQuery | PHP</p>
-						<div class="box">
-							<a href="http://hanshugos.se/" target="_blank">
-								<img src="assets/img/hanshugos.se_responsive.jpg" />
-							</a>
-						</div>
-					</div>
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="https://github.com/smallcolin/wine" target="_blank">
-								wine ecommerce
-							</a>
-						</h4>
-						<p class="move-text">Laravel | PHP </p>
-						<div class="box">
-							<a href="https://github.com/smallcolin/wine" target="_blank">
-								<img src="assets/img/wine-test.jpg" />
-							</a>
-						</div>
-					</div>
-					<div class="site-container">
-						<h4 class="animated flipInX">
-							<a href="http://smallcolin.se/" target="_blank">
-								smallcolin.se
-							</a>
-						</h4>
-						<p class="move-text">HTML / CSS / JQuery</p>
-						<div class="box">
-							<a href="http://smallcolin.se/" target="_blank">
-								<img src="assets/img/smallcolin.se_responsive.jpg" />
-							</a>
-						</div>
-					</div>
+					<?php } ?>
 				</div>
 			</div>
 		</div>
