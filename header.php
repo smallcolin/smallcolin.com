470cead4760adb114ba70d3e22f1232bb07dafed
smallcolin <colinsweeney2000@gmail.com>
2018-11-28 21:05:14 +0100

    added secure https redirect

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
 <?php
+	//force redirect to secure page
+	if($_SERVER['SERVER_PORT'] != '443') { header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']); exit(); }
+		
 	include "includes/include.php";
 
 	$emailError = "";
